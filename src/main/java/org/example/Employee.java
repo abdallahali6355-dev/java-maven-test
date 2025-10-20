5cb4ea169f77057e2831671f2646aefaaff7a446
abdallah6355 <abdallah6355@gmail.com>
2025-10-21 00:12:11 +0300

    Employee class

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,58 @@
+package org.example;
+
+public class Employee {
+private int id;
+private String name,adress,jobName;
+private double salary;
+
+    public Employee(int id, String name, String adress, String jobName, double salary) {
+        this.id = id;
+        this.name = name;
+        this.adress = adress;
+        this.jobName = jobName;
+        this.salary = salary;
+    }
+
+    public Employee() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
